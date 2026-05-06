2deda1e253afd806ffac95e965457fd3ce6f16ac
jiancpp <jiannaclarisse.tm@gmail.com>
2026-05-06 16:12:46 +0800

    Fix deployment error

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
   // Catch-all route to serve index.html
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
   });
 } else {
